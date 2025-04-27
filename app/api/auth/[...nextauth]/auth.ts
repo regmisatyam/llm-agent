5606b22170e7a9f5126020822e97ab1b347e551c
regmisatyam <info@satyamregmi.com.np>
2025-04-27 10:19:38 -0400

    changesecure

--- app/api/auth/[...nextauth]/auth.ts
+++ app/api/auth/[...nextauth]/auth.ts
@@ -94,7 +94,7 @@ export const authOptions: NextAuthOptions = {
         // httpOnly: true,
         sameSite: "lax",
         path: "/",
-        secure: false,
+        secure: true,
       },
     },
   },
